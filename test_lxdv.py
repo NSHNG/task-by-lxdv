26397d3df00983aac394a97a49496eff1ac1edde
NSHNG <32848431+NSHNG@users.noreply.github.com>
2018-12-14 11:43:34 +0300

    Edit any code.

--- test_lxdv.py
+++ test_lxdv.py
@@ -1,18 +1,13 @@
 import argparse
-import sys
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--degree', default = '2')
+parser.add_argument('--degree', default = 2, type = 'int')
 
-FLAGS = parser.parse_args()
+args = parser.parse_args()
 
 def deg(x):
-    return (x**int(FLAGS.degree))
+    return (x**int(args.degree))
     
-
-if __name__ == "__main__":
-    print('Server is started with following params:\n \
-    degree: {0}\n \
-    '.format(FLAGS.degree))
-    x = int(input())
-    print(deg(x))
+main()
+x = int(input())
+print(deg(x))
